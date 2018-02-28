d93c431bd0e5cb3c704340ba7fb8ee1ba82fda0b
Luke Przekwas <galactica@The-Galactica.local>
2018-02-28 13:43:34 -0600

    route for single type added

--- server/src/routes/type.js
+++ server/src/routes/type.js
@@ -4,6 +4,7 @@ import Table from '../table';
 let router = Router();
 let typeTable = new Table('type');
 
+//GET route for all types
 router.get('/', (req, res) => {
     console.log(req.user);
     typeTable.getAll()
@@ -13,6 +14,21 @@ router.get('/', (req, res) => {
         console.log(err);
         res.sendStatus(500);
     });
+});
+
+//GET route for single type
+router.get('/:id', (req, res) => {
+
+    let id = req.params.id;
+
+     typeTable.getOne(id)
+    .then((results) => {
+        res.json(results);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+
 });
 
 export default router;
