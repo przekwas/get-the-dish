97e57a496acd64c373f067577e5ce2bf92d32016
Luke Przekwas <galactica@The-Galactica.local>
2018-03-07 10:24:47 -0600

    testing yelp results

--- server/src/routes/yelp.js
+++ server/src/routes/yelp.js
@@ -7,16 +7,15 @@ let router = Router();
 const client = yelp.client(apiKey);
 
 const searchRequest = {
-  term:'urban standard',
-  location: 'birmingham, al'
+  term:'birmingham, al'
 };
 
-//GET route for latest 3 items added into food_items table by their _created date
+
 router.get('/search', (req, res) => {
   
-  client.search(searchRequest).then(response => {
+  client.search(searchRequest)
+  .then(response => {
     const firstResult = response.jsonBody.businesses[0];
-    // const prettyJson = JSON.stringify(firstResult, null, 4);
     res.json(firstResult);
   }).catch(e => {
     console.log(e);
