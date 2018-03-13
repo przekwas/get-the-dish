73791bf1342e78cfbb941f8668f785006b868c83
Luke Przekwas <galactica@The-Galactica.local>
2018-03-13 14:48:17 -0500

    testing second yelp api call

--- server/src/routes/yelp.js
+++ server/src/routes/yelp.js
@@ -6,21 +6,29 @@ const apiKey = process.env.YELP_KEY;
 let router = Router();
 const client = yelp.client(apiKey);
 
-const searchRequest = {
-  term: 'food',
-  latitude: '33.543682',
-  longitude: '-86.779633',
-  radius: '40000',
-  limit: '50'
-};
-
 //Route to get Birmingham, AL business to populate dropdown on front end
 router.get('/search', (req, res) => {
 
+  let searchRequest = {
+    term: 'food',
+    latitude: '33.543682',
+    longitude: '-86.779633',
+    radius: '40000',
+    limit: '50',
+    offset: '0'
+  };
+
   client.search(searchRequest)
     .then(response => {
-      const firstResult = response.jsonBody.businesses;
-      res.json(firstResult);
+      const firstResult = response.jsonBody.businesses
+      searchRequest.offset = '51'
+        .then(client.search(searchRequest)
+          .then((nextResponse => {
+            const secondResult = nextResponse.jsonBody.businesses
+            firstResult.push(secondResult)
+            res.json(firstResult);
+          })))
+      // res.json(firstResult)
     }).catch(e => {
       console.log(e);
       res.sendStatus(500);
